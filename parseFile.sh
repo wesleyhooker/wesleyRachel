a8e49503117339cf59f087e10093d088ad5197d9
Wesley Hooker <wesleyhooker@mail.weber.edu>
2018-10-01 15:10:14 +0000

    initialcommit

---
+++ parseFile.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -
+#===============================================================================
+#
+#          FILE: parseFile.sh
+#
+#         USAGE: ./parseFile.sh
+#
+#   DESCRIPTION: 
+#
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: Wesley Hooker (Whooker), wesleyhooker@mail.weber.edu
+#  ORGANIZATION: WSU
+#       CREATED: 10/01/2018 03:09:42 PM
+#      REVISION:  ---
+#===============================================================================
+
+set -o nounset                                  # Treat unset variables as an error
+

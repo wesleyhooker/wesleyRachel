f4d4b151ba36574fd8be0eb673007be32663fdcd
Rachel Laub <rachellaub@mail.weber.edu>
2018-10-04 16:06:24 +0000

    Modified main

--- main.sh
+++ main.sh
@@ -23,10 +23,31 @@ have_f=0
 have_l=0
 have_e=0
 
-#calls program 1
+#calls program 1 to retrieve files
 systemCalls1()
 {
+    echo "Calls program 1"
     #for (int i = firstYear)
+}
+#calls program 2 to expand files downloaded
+systemCalls2()
+{
+    echo "Calls program 2"
+}
+#calls program 3 to filter data
+systemCalls3()
+{
+    echo "Calls program 3"
+}
+#calls program 4 to compress data
+systemCalls4()
+{
+    echo "Calls program 4"
+}
+#calls program 5 to FTP file to server
+systemCalls5()
+{
+    echo "Calls program 5"
 }
 
 

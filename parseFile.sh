65a7180992e49514e9219a189235155b75dbd609
Wesley Hooker <wesleyhooker@mail.weber.edu>
2018-10-09 22:57:37 +0000

    got rid of sed

--- parseFile.sh
+++ parseFile.sh
@@ -22,17 +22,13 @@ set -o nounset                                  # Treat unset variables as an er
 echo >> log
 echo "PROGRAM 3:" >> log
 
-#replace empty email fields with null
-#finds empty email strings in MOCK_DATA files
-#replaces empty email strings with default waldo email
-echo "Finding and replacing empty email values" >> log
-`sed -i 's/,,/,waldo@weber.edu, /g' temp/MOCK_DATA*.csv`
-
+#REPLACE EMPTY EMAIL FIELD WITH WALDO EMAIL
 #only canadian female residents
 #awk delimiter = ,
 #if female and from canada
 #print first, last, email
 #file to be awked = temp/MOCK_DATA*.csv
 #output saved to temp/canadianFemales.csv
+echo "Finding and replacing empty email values" >> log
 echo "Printing canadian females to output file \"canadianFemales.csv\"" >> log
-`awk -F',' '{if($4 == ""){$4=fart}if($5 == "Female" && $6 == "Canada"){print $2", "$3", "$4} }' temp/MOCK_DATA*.csv > temp/canadianFemales.csv`
+`awk -F',' '{if($4 == ""){$4="waldo@weber.edu"}if($5 == "Female" && $6 == "Canada"){print $2", "$3", "$4} }' temp/MOCK_DATA*.csv > temp/canadianFemales.csv`
